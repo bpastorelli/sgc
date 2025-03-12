35c3bf53ff95e0b82538cccec02dbb5309efa712
bpastorelli <54052030+bpastorelli@users.noreply.github.com>
2025-03-12 13:11:11 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,14 +21,6 @@ export class AppComponent {
 
   ngOnInit() {
     this.router.navigate([''])
-      this.router.get('/', function(req, res) {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
-      res.setHeader('Access-Control-Allow-Credentials', true); // If needed
-  
-      res.send('cors problem fixed:)');
-    });
   }
 
   logout() {
