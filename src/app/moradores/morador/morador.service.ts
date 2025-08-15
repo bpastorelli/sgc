9bdc52733938ad271cf83ebb987c4bfc6a273fdf
bpastorelli <54052030+bpastorelli@users.noreply.github.com>
2025-08-14 22:34:55 -0300

    Update morador.service.ts

--- src/app/moradores/morador/morador.service.ts
+++ src/app/moradores/morador/morador.service.ts
@@ -64,7 +64,7 @@ private moradorUrl = environment.protocol + environment.urlCloud + environment.u
         queryParams = this.setParameter(this.moradorRequest);
     }
 
-    return this.http.get<Array<MoradorResponse>>(moradorUrl + environment.filtro, {params: queryParams})
+    return this.http.get<Array<MoradorResponse>>(this.moradorUrl + environment.filtro, {params: queryParams})
         .pipe(
           map(response => response)
         );
