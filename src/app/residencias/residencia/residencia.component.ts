fe1210b5295ad44af2b4f520a1d3a2c053768b46
bpastorelli <54052030+bpastorelli@users.noreply.github.com>
2025-08-16 11:00:43 -0300

    Update residencia.component.ts

--- src/app/residencias/residencia/residencia.component.ts
+++ src/app/residencias/residencia/residencia.component.ts
@@ -127,6 +127,7 @@ export class ResidenciaComponent implements OnInit {
         this.residencia = data;
         this.acao = 'view';
         this.open('customModal1');
+        console.log(this.residencia.ticket);
         this.getResidenciaByTicket(this.residencia.ticket);
         this.router.navigate(['/residencia/view/', this.residencias[0].id]);
       },err=>{
