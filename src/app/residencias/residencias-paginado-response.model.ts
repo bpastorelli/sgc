c0069c8c3e0e2af84640e65b6383e2c351f08caf
bpastorelli <54052030+bpastorelli@users.noreply.github.com>
2025-08-16 12:55:26 -0300

    Update residencias-paginado-response.model.ts

--- src/app/residencias/residencias-paginado-response.model.ts
+++ src/app/residencias/residencias-paginado-response.model.ts
@@ -1,7 +1,7 @@
 import { Paginacao } from "src/app/_models/paginacao";
 import { Residencia } from "./residencias.model";
 
-export class ResidenciasPaginadoResponse{
+export interface ResidenciasPaginadoResponse{
 
     residencias: Residencia[],
     
